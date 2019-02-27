54b461c22aab1b51810d648dfdcd8cbcb2afede2
visualstudio <visualstudio@122-PC01>
2019-02-27 09:12:32 -0500

    few things

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,9 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            var orderProcessor = new OrderProcessor();
+            var order = new OrderedParallelQuery { DatePlaced = DateTime.Now, TotalPrice = 100f }
+            orderProcessor.Process();
         }
     }
 }
